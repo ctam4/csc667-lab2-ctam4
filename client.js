617104284952eb1f03bff39ac4bb87de7c6a2d08
Calvin Tam <51683307+ctam4@users.noreply.github.com>
2020-02-12 20:35:54 -0800

    coursework

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
-console.log('I am client');
+const axios = require('axios');
+
+axios.get('http://localhost:3000')
+.then((res) => {
+  console.log(res.data);
+})
+.catch(e => console.log(e));
+
+const body = {
+  hello: 'world',
+  stuff: [5, 6, 7],
+};
+
+axios.post('http://localhost:3000/post-test', body)
+.then((res) => {
+  console.log(res.data);
+})
+.catch(e => console.log(e));
--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('I am server');
+const express = require('express');
+
+const app = express();
+app.use(express.json()); // this is a middleware
+
+app.use((req, res, next) => {
+  // custom middleware
+  console.log('Incomeing request!');
+  console.log(req.originalUrl);
+  console.log(req.headers['user-agent']);
+  next();
+});
+
+app.get('/calvin', (req, res) => {
+  console.log(req.query);
+  res.send(`Hello, ${req.query.hello}`);
+});
+
+app.post('/post-test', (req, res) => {
+  console.log(req.body);
+  res.send('Worked!');
+});
+
+app.get('*', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(3000);
